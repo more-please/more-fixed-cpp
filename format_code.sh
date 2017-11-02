211381f5178c3c63311f4a71d84b3e33d4b37b6b
Iain Merrick <iain@iainmerrick.com>
2017-11-02 11:50:48 -0500

    Fix format_code.sh (ignore CMake output)

--- format_code.sh
+++ format_code.sh
@@ -4,9 +4,7 @@ set -e
 
 cd `dirname $0`
 
-for ext in "*.h" "*.cpp"; do
-	for f in `find . -name $ext`; do
-		echo "Formatting $f"
-		clang-format -style=file -i $f
-	done
+for f in `find -E . -regex ".*\.(h|cpp)" | grep -v CMake`; do
+	echo "Formatting $f"
+	clang-format -style=file -i $f
 done
